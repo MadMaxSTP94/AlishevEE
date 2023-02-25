d8db61bb312490dd627e5214a714f00268216329
Metal <metal@mail.com>
2023-02-25 23:24:38 +0300

    Some changes

--- proj-1/src/main/java/FirstServlet.java
+++ proj-1/src/main/java/FirstServlet.java
@@ -3,12 +3,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        httpServletRequest.getRequestDispatcher("page1.jsp").forward(httpServletRequest,httpServletResponse);
+        //httpServletRequest.getRequestDispatcher("page1.jsp").forward(httpServletRequest,httpServletResponse);
+        /*
+
+        * */
+        PrintWriter pw = httpServletResponse.getWriter();
+
+        pw.println("<html><h1>hello world</h1></html>");
     }
 
     @Override
