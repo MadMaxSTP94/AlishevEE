ad76e5c59bd2f2075b585f2b9cd828e32e3b4353
MadMaxSTP_94 <metallicap96@mail.ru>
2023-02-26 22:40:43 +0300

    last changes

--- proj-1/src/main/java/CookieServlet.java
+++ proj-1/src/main/java/CookieServlet.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,5 +15,15 @@ public class CookieServlet extends HttpServlet {
         Если очистить куки, сервер не сможет узнать его сессию
 
         */
+
+        // Назначаем куки
+        Cookie cookie1 = new Cookie("some_id","123");
+        Cookie cookie2 = new Cookie("some_name", "Alesha" );
+
+        cookie2.setMaxAge(24 * 60 * 60);
+        cookie1.setMaxAge(24 * 60 * 60); // срок действия куки в секундах
+        //добавляем куки в браузер клиента
+        response.addCookie(cookie1);
+        response.addCookie(cookie2);
     }
 }
